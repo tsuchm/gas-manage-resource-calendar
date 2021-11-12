2d0f03c26f0cfff281d22ec0e8e112ea215bf403
TSUCHIYA Masatoshi <tsuchiya@imc.tut.ac.jp>
2021-11-12 21:48:23 +0900

    Implement gentle log messages and log notification.

--- code.js
+++ code.js
@@ -9,15 +9,30 @@ const calendarAdministratorGroup = '';
 
 const headerRow = 1;
 
+const logNotifyAddress = calendarAdministratorGroup;
+
+let logMessages = [];
+
 // main
 function updateResourcesAndBuildings(){
-  const book = SpreadsheetApp.openById(bookId);
-  updateBuildings(book);
-  updateResources(book);
-  //deleteResources();
+  logMessages = [];
+  updateBuildings();
+  updateResources();
+  deleteResources();
+  if (logMessages.length > 0){
+    if (logNotifyAddress) {
+      GmailApp.sendEmail(logNotifyAddress, `Resource Calendar Management Result for ${domainName}`, logMessages.join("\n"));
+    }
+  }
 }
 
-function updateBuildings(book){
+function pushLogMessage(msg){
+  logMessages.push(msg);
+  Logger.log('%s', msg);
+}
+
+function updateBuildings(){
+  const book = openBook();
   const sheet = book.getSheetByName('Buildings');
   const dict = getHeader(sheet, headerRow);
   const last = sheet.getLastRow();
@@ -27,6 +42,13 @@ function updateBuildings(book){
     let floors = sheet.getRange(row, dict['Floors']).getValue().toString().split(',');
     updateBuilding(row, id, name, floors);
   }
+}
+
+function openBook(){
+  if (! openBook.cache) {
+    openBook.cache = SpreadsheetApp.openById(bookId);
+  }
+  return openBook.cache;
 }
 
 function getHeader(sheet, row){
@@ -40,15 +62,15 @@ function getHeader(sheet, row){
 
 function updateBuilding(row, buildingId, buildingName, floorNames){
   if(! buildingId){
-    Logger.log("%s行: 建物IDは省略できません", row);
+    pushLogMessage(`建物IDは省略できません: row=${row}`)
     return;
   }
   if(! buildingName){
-    Logger.log("%s行: 建物名は省略できません", row);
+    pushLogMessage(`建物名は省略できません: row=${row}, buildingId=${buildingId}`);
     return;
   }
   if(! floorNames){
-    Logger.log("%s行: 階数の指定は省略できません", row);
+    pushLogMessage(`階数の指定は省略できません: row=${row}, buildingId=${buildingId}, buildingName=${buildingName}`)
     return;
   }
   let building;
@@ -58,9 +80,20 @@ function updateBuilding(row, buildingId, buildingName, floorNames){
     // Do nothing.
   }
   if(building){
-    building.buildingName = buildingName;
-    building.floorNames = floorNames;
-    AdminDirectory.Resources.Buildings.update(building, customerId, buildingId);
+    let change = 0;
+    if (building.buildingName != buildingName) {
+      pushLogMessage(`建物名が更新されました: buildingId=${buildingId}, old=${building.buildingName}, new=${buildingName}`);
+      building.buildingName = buildingName;
+      change++;
+    }
+    if (building.floorNames.toString() != floorNames.toString()) {
+      pushLogMessage(`階数が更新されました: buildingId=${buildingId}, buildingName=${buildingName}, old=${building.floorNames}, new=${floorNames}`);
+      building.floorNames = floorNames;
+      change++;
+    }
+    if (change > 0) {
+      AdminDirectory.Resources.Buildings.update(building, customerId, buildingId);
+    }
   } else {
     building = {
       buildingId: buildingId,
@@ -71,12 +104,14 @@ function updateBuilding(row, buildingId, buildingName, floorNames){
   }
 }
 
-function updateResources(book){
+function updateResources(){
+  const book = openBook();
   const sheet = book.getSheetByName('Resources');
   const dict = getHeader(sheet, headerRow);
   const last = sheet.getLastRow();
   for (let row=headerRow + 1; row<=last; row++){
     let id = normalizeResourceId(sheet.getRange(row, dict['Resource Id']).getValue().toString());
+    //let owner = sheet.getRange(row, dict['Resource Owner']).getValue().toString();
     let group = getResourceUserGroup(sheet.getRange(row, dict['Resource User']).getValue().toString());
     let name = sheet.getRange(row, dict['Resource Name']).getValue().toString();
     let building = getBuildingId(sheet.getRange(row, dict['Building Name']).getValue().toString());
@@ -95,23 +130,23 @@ function updateResources(book){
 
 function updateResource(row, resourceId, resourceGroup, resourceName, buildingId, resourceCategory, resourceType, floorName, capacity, floorSection){
   if(! resourceName){
-    Logger.log("%d行: リソース名は省略できません", row);
+    pushLogMessage(`リソース名は省略できません: row=${row}`);
     return;
   }
   if(! resourceCategory){
-    Logger.log("%d行: リソース種別は省略できません", row);
+    pushLogMessage(`リソース種別は省略できません: row=${row}, resourceName=${resourceName}`);
     return;
   }
   if(! buildingId){
-    Logger.log("%d行: 建物名は省略できません", row);
+    pushLogMessage(`建物名は省略できません: row=${row}, resourceName=${resourceName}`);
     return;
   }
   if(floorName && !validateFloorName(buildingId, floorName)){
-    Logger.log("%d行: 指定された階は存在しません", row);
+    pushLogMessage(`指定された階は存在しません: row=${row}, resourceName=${resourceName}, floorName=${floorName}`);
     return;
   }
   if(resourceCategory == 'CONFERENCE_ROOM' && !capacity){
-    Logger.log("%d行: 会議室の収容人数は省略できません", row);
+    pushLogMessage(`会議室の定員は省略できません: row=${row}, resourceName=${resourceName}`);
     return;
   }
   let resource = false;
@@ -121,14 +156,45 @@ function updateResource(row, resourceId, resourceGroup, resourceName, buildingId
     // Do nothing.
   }
   if (resource) {
-    resource.resourceName = resourceName;
-    resource.buildingId = buildingId;
-    resource.resourceCategory = resourceCategory;
-    resource.resourceType = resourceType;
-    resource.floorName = floorName;
-    resource.floorSection = floorSection;
-    resource.capacity = capacity;
-    AdminDirectory.Resources.Calendars.update(resource, customerId, resourceId);
+    let change = 0;
+    if (resource.resourceName != resourceName) {
+      pushLogMessage(`リソース名が更新されました: resourceId=${resourceId}, old=${resource.resourceName}, new=${resourceName}`);
+      resource.resourceName = resourceName;
+      change++;
+    }
+    if (resource.buildingId != buildingId) {
+      pushLogMessage(`リソースの建物IDが更新されました: resourceId=${resourceId}, resourceName=${resourceName}, old=${resource.buildingId}, new=${buildingId}`);
+      resource.buildingId = buildingId;
+      change++;
+    }
+    if (resource.resourceCategory != resourceCategory) {
+      pushLogMessage(`リソースの分類が更新されました: resourceId=${resourceId}, resourceName=${resourceName}, old=${resource.resourceCategory}, new=${resourceCategory}`);
+      resource.resourceCategory = resourceCategory;
+      change++;
+    }
+    if (! safeStringEqual(resource.resourceType, resourceType)) {
+      pushLogMessage(`リソースの種別が更新されました: resourceId=${resourceId}, resourceName=${resourceName}, old=${resource.resourceType}, new=${resourceType}`);
+      resource.resourceType = resourceType;
+      change++;
+    }
+    if (! safeStringEqual(resource.floorName, floorName)) {
+      pushLogMessage(`リソースの建物階が更新されました: resourceId=${resourceId}, resourceName=${resourceName}, old=${resource.floorName}, new=${floorName}`);
+      resource.floorName = floorName;
+      change++;
+    }
+    if (! safeStringEqual(resource.floorSection, floorSection)) {
+      pushLogMessage(`リソースの建物区画が更新されました: resourceId=${resourceId}, resourceName=${resourceName}, old=${resource.floorSection}, new=${floorSection}`);
+      resource.floorSection = floorSection;
+      change++;
+    }    
+    if (! safeNumberEqual(resource.capacity, capacity)) {
+      pushLogMessage(`リソースの定員が更新されました: resourceId=${resourceId}, resourceName=${resourceName}, old=${resource.capacity}, new=${capacity}`);
+      resource.capacity = capacity;
+      change++;
+    }
+    if (change > 0) {
+      AdminDirectory.Resources.Calendars.update(resource, customerId, resourceId);
+    }
   } else {
     const newId = createResourceId();
     resource = AdminDirectory.Resources.Calendars.insert({
@@ -141,6 +207,7 @@ function updateResource(row, resourceId, resourceGroup, resourceName, buildingId
       floorSection: floorSection,
       capacity: capacity
     }, customerId);
+    pushLogMessage(`リソースを作成しました: resourceId=${newId}, resourceName=${resourceName}, calendarId=${resource.resourceEmail}, resourceGroup=${resourceGroup}`);
   }
   // リソースが作成されるまで待機
   let cal = false;
@@ -152,10 +219,31 @@ function updateResource(row, resourceId, resourceGroup, resourceName, buildingId
     }
   } while(! cal);
   cal.setTimeZone("Asia/Tokyo");
-  Logger.log('Resource updated: resourceName=%s, resourceId=%s, calendarId=%s, calendarTimezone=%s, resourceGroup=%s', resource.resourceName, resource.resourceId, resource.resourceEmail, cal.getTimeZone(), resourceGroup);
-  resetResourceCalendarACL(resource.resourceEmail, resourceGroup);
-  showResourceCalendarACL(resource.resourceEmail);
+  updateResourceCalendarACL(resource, resourceGroup);
+  //showResourceCalendarACL(resource);
   return resource;
+}
+
+// undefined を空文字列に変換してから厳密な等価比較
+function safeStringEqual(x, y) {
+  if (x === void 0) {
+    x = '';
+  }
+  if (y === void 0) {
+    y = '';
+  }
+  return x === y;
+}
+
+// undefined を0に変換してから厳密な等価比較
+function safeNumberEqual(x, y) {
+  if (x === void 0) {
+    x = 0;
+  }
+  if (y === void 0) {
+    y = 0;
+  }
+  return x === y;
 }
 
 function deleteResources(){
@@ -164,7 +252,7 @@ function deleteResources(){
     let id = lookupSheet(Number(resource.resourceId), 'Resources', 'Resource Id', 'Resource Id');
     if(! id){
       AdminDirectory.Resources.Calendars.remove(customerId, resource.resourceId);
-      Logger.log('Resource removed: resourceName=%s, resourceId=%s, calendarId=%s', resource.resourceName, resource.resourceId, resource.resourceEmail);
+      pushLogMessage(`リソースを削除しました: resourceId=${resource.resourceId}, resourceName=${resource.resourceName}, calendarId=${resource.resourceEmail}`);
     }
   });
 }
@@ -214,7 +302,7 @@ function getResourceUserGroup(name){
 }
 
 function lookupSheet(target, sheetName, keyName, valueName) {
-  const book = SpreadsheetApp.openById(bookId);
+  const book = openBook();
   const sheet = book.getSheetByName(sheetName);
   const header = sheet.getRange(headerRow, 1, 1, sheet.getMaxRows()).getValues().shift();
   const keyColumn = header.indexOf(keyName) + 1;
@@ -256,31 +344,40 @@ function showAllResources() {
   let resources = listAllResources();
   resources.forEach(function(resource){
     Logger.log('%s (%s, %s)', resource.resourceName, resource.resourceId, resource.resourceEmail);
-    showResourceCalendarACL(resource.resourceEmail);
+    showResourceCalendarACL(resource);
   })
 }
 
 function resetAllResourceCalendarACL() {
   let resources = listAllResources();
   resources.forEach(function(resource){
-    Logger.log('%s (%s, %s)', resource.resourceName, resource.resourceId, resource.resourceEmail);
-    resetResourceCalendarACL(resource.resourceEmail, false);
-    showResourceCalendarACL(resource.resourceEmail);
+    updateResourceCalendarACL(resource, false);
+    showResourceCalendarACL(resource);
   })
 }
 
-function resetResourceCalendarACL(calendarId, resourceGroup) {
-  setResourceCalndarACL(calendarId, 'default', '__public_principal__@public.calendar.google.com', 'none');
-  setResourceCalndarACL(calendarId, 'domain', domainName, 'none');
+function updateResourceCalendarACL(resource, resourceGroup) {
+  const calendarId = resource.resourceEmail;
+  let validAclIds = ['user:'+calendarId];
+  validAclIds.push(setResourceCalndarACL(resource, 'default', '__public_principal__@public.calendar.google.com', 'none'));
+  validAclIds.push(setResourceCalndarACL(resource, 'domain', domainName, 'none'));
   if (resourceGroup) {
-    setResourceCalndarACL(calendarId, 'group', resourceGroup, 'writer');
+    validAclIds.push(setResourceCalndarACL(resource, 'group', resourceGroup, 'writer'));
   }
   if (calendarAdministratorGroup) {
-    setResourceCalndarACL(calendarId, 'group', calendarAdministratorGroup, 'owner');
+    validAclIds.push(setResourceCalndarACL(resource, 'group', calendarAdministratorGroup, 'owner'));
   }
+  const acls = listResourceCalendarACL(resource);
+  acls.forEach(function(acl){
+    if (validAclIds.indexOf(acl.id) < 0) {
+      Calendar.Acl.remove(calendarId, acl.id, {sendNotifications: false});
+      pushLogMessage(`ACLが削除されました: resourceId=${resource.resourceId}, resourceName=${resource.resourceName}, scopeType=${acl.scope.type}, scopeValue=${acl.scope.value}`);
+    }
+  });
 }
 
-function setResourceCalndarACL(calendarId, scopeType, scopeValue, role) {
+function setResourceCalndarACL(resource, scopeType, scopeValue, role) {
+  const calendarId = resource.resourceEmail;
   let acl;
   try {
     acl = Calendar.Acl.get(calendarId, scopeType+':'+scopeValue);
@@ -288,30 +385,39 @@ function setResourceCalndarACL(calendarId, scopeType, scopeValue, role) {
     // Do nothing.
   }
   if (acl) {
-    acl.role = role;
-    Calendar.Acl.update(acl, calendarId, acl.id, {sendNotifications: false});
+    if (acl.role != role) {
+      Calendar.Acl.update(acl, calendarId, acl.id, {sendNotifications: false});
+      pushLogMessage(`ACLが更新されました: resourceId=${resource.resourceId}, resourceName=${resource.resourceName}, scopeType=${scopeType}, scopeValue=${scopeValue}, old=${acl.role}, new=${role}`);
+    }
   } else {
     if (role != 'none') {
-      Calendar.Acl.insert({
+      acl = Calendar.Acl.insert({
         scope: {
           type: scopeType,
           value: scopeValue
         }, 
         role: role
       }, calendarId, {sendNotifications: false});
+      pushLogMessage(`ACLが作成されました: resourceId=${resource.resourceId}, resourceName=${resource.resourceName}, scopeType=${scopeType}, scopeValue=${scopeValue}, role=${role}`);
     }
+  }
+  if (acl) {
+    return acl.id;
+  } else {
+    return null;
   }
 }
 
-function showResourceCalendarACL(calendarId) {
-  let acls = listResourceCalendarACL(calendarId);
+function showResourceCalendarACL(resource) {
+  const acls = listResourceCalendarACL(resource);
   acls.forEach(function(acl){
     Logger.log('scopeType=%s, scopeValue=%s, role=%s', acl.scope.type, acl.scope.value, acl.role);
   })
 }
 
 // https://developers.google.com/calendar/v3/reference/acl
-function listResourceCalendarACL(calendarId) {
+function listResourceCalendarACL(resource) {
+  const calendarId = resource.resourceEmail;
   let pageToken;
   let acls = [];
   do {
